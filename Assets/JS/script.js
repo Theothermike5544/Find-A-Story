8e36113a04765ccabad71252a5b51469f7c5bffb
Leocrydis <82065055+Leocrydis@users.noreply.github.com>
2022-07-22 20:35:36 -0400

    test

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,37 @@
-const bookAPIKey = "AIzaSyAWyw1BzmR-6qJOC2A1-G0l_CqKVCN8N4s"
-const movieAPIKey = "ec8e125d"
+var submit = document.getElementById('submit-btn')
+var searchValue = document.getElementById('search-song')
 
+//var apiKey = "97d82ea7f7308c579a185e2066658d2a"
+
+var lyricsResults ="http://tracking.musixmatch.com/t1.0/AMa6hJCIEzn1v8RuOP"
+
+
+//adding event listener for submit btn
+submit.addEventListener("submit-btn", searchSong);
+
+function searchSong(){
+    document.getElementById('search-song'); 
+}
+
+var stringLyrics = JSON.stringify(lyricsResults);
+console.log(lyricsResults, stringLyrics);
+
+var displayLyrics = function(){
+    var text = "";
+    var lyrics = localStorage.getItem(lyricsResults);
+    var lyricsArry = JSON.parse(lyrics);
+    for (var i=0; i < lyricsArry.length; i++){
+        text += lyrics[i] + "<br>";
+    };
+    document.getElementById('search-results').innerHTML = text;
+}
+
+// const music = require('musicmatch')({ apikey: "MY_API KEY" });
+
+// music.trackSearch({ q: "Drake - God's Plan", page: 1, page_size: 3 })
+//   .then(function (data) {
+//     console.log(data.message.body.track_list);
+
+//   }).catch(function (err) {
+//     console.log(err);
+//   })
